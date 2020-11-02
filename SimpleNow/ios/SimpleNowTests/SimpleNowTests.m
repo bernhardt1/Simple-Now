60807da902b0aa031c9dfd4abf8f765323620dcb
Jason Bernhardt <jason_bernhardt_12@hotmail.com>
2020-11-01 16:50:44 -0800

    renamed to simple now

--- SimpleNow/ios/SimpleNowTests/SimpleNowTests.m
+++ SimpleNow/ios/SimpleNowTests/SimpleNowTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PracticeTests : XCTestCase
+@interface SimpleNowTests : XCTestCase
 
 @end
 
-@implementation PracticeTests
+@implementation SimpleNowTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
